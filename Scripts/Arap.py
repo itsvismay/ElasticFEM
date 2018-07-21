FINALLY fixed the arap jacobian bugs

Scripts/Arap.py
@@ -86,12 +86,13 @@ class ARAP:
 		PAg = self.mesh.getP().dot(self.mesh.getA().dot(self.mesh.G.dot(_z) + self.mesh.x0)) 
 		FPAx = _R.dot(_U.dot(_S.dot(_U.T.dot(self.PAx))))
 		# print(PAg)
-		# print(FPAx)
+		# print(self.mesh.red_r)
+		# print(self.mesh.red_s)
 		return 0.5*(np.dot(PAg - FPAx, PAg - FPAx))
 
 	def Energy(self):
 		PAg = self.PA.dot(self.mesh.getg())
-		FPAx0 = self.mesh.GF.dot(self.PAx) #self.constTimeFPAx()
+		FPAx0 = self.constTimeFPAx() #self.mesh.GF.dot(self.PAx)
 		en = 0.5*(np.dot(PAg - FPAx0, PAg - FPAx0))
 		return en
 
@@ -155,14 +156,14 @@ class ARAP:
 		# s_size = len(self.mesh.red_s)
 		# DR = self.sparseDRdr()
 		# DS = self.sparseDSds()
-
 		Ezz = self.Egg
+
 		
 		# ###############ERG
-		# # sample = self.sparseErg_first(-1*self.PAG, USUtPAx)
-		# # for j in range(self.Erg.shape[1]):
-		# # 	for i in range(self.Erg.shape[0]):
-		# # 		self.Erg[i,j] = DR[j].multiply(sample[i]).sum()
+		# sample = self.sparseErg_first(-1*sparse.csc_matrix(self.PAG), USUtPAx)
+		# for j in range(self.Erg.shape[1]):
+		# 	for i in range(self.Erg.shape[0]):
+		# 		self.Erg[i,j] = DR[j].multiply(sample[i]).sum()
 		self.Erg = self.constTimeErz()
 
 
@@ -183,6 +184,7 @@ class ARAP:
 		# 		self.Egs[i,j] = DS[j].multiply(d_gEgdS[i]).sum()
 		# print(self.Egs)
 		self.Egs = self.constTimeEgs()
+		
 		# print(self.Egs)
 		# exit()
 		mid = self.constTimeErs_mid()
@@ -455,6 +457,13 @@ class ARAP:
 		MUtPAxsW = MUtPAx.dot(self.mesh.sW)
 		DSUtPAxMUtPAxsW = MUtPAxsW.T.dot(self.constErs_Terms[0])
 		self.constEsTerms.append(DSUtPAxMUtPAxsW)
+		
+
+		# print(MUtPAxsW.dot(self.mesh.red_s) - self.mesh.GS.dot(UtPAx))
+		# DS = self.sparseDSds()
+		# out = self.sparseOuterProdDiags(self.mesh.GS.dot(UtPAx), UtPAx)
+		# print(DS[0].multiply(out).sum())
+		# exit()
 
 		U_PAx = sparse.lil_matrix((6*len(self.mesh.T), 2*len(self.mesh.T)))
 		for t in range(len(self.mesh.T)):
@@ -584,26 +593,25 @@ class ARAP:
 			MUtPAx[6*t+0, 3*t+2] = x[1]*u1 - x[0]*u2
 			MUtPAx[6*t+1, 3*t+0] = x[0]*u2
 			MUtPAx[6*t+1, 3*t+1] = x[1]*u1
-			MUtPAx[6*t+1, 3*t+2] = x[0]*u1 - x[1]*u2
+			MUtPAx[6*t+1, 3*t+2] = x[0]*u1 + x[1]*u2
 
 			MUtPAx[6*t+2, 3*t+0] = x[2]*u1
 			MUtPAx[6*t+2, 3*t+1] = -x[3]*u2
 			MUtPAx[6*t+2, 3*t+2] = x[3]*u1 - x[2]*u2
 			MUtPAx[6*t+3, 3*t+0] = x[2]*u2
 			MUtPAx[6*t+3, 3*t+1] = x[3]*u1
-			MUtPAx[6*t+3, 3*t+2] = x[2]*u1 - x[3]*u2
+			MUtPAx[6*t+3, 3*t+2] = x[2]*u1 + x[3]*u2
 			
 			MUtPAx[6*t+4, 3*t+0] = x[4]*u1
 			MUtPAx[6*t+4, 3*t+1] = -x[5]*u2
 			MUtPAx[6*t+4, 3*t+2] = x[5]*u1 - x[4]*u2
 			MUtPAx[6*t+5, 3*t+0] = x[4]*u2
 			MUtPAx[6*t+5, 3*t+1] = x[5]*u1
-			MUtPAx[6*t+5, 3*t+2] = x[4]*u1 - x[5]*u2
+			MUtPAx[6*t+5, 3*t+2] = x[4]*u1 + x[5]*u2
 
 		MUtPAx.tocsc()
 		MUtPAxsW = MUtPAx.dot(self.mesh.sW)
-
-		wr_cols = []
+		
 		for i in range(len(self.mesh.red_r)):
 			B = self.mesh.RotationBLOCK[i]
 			self.constItRTerms.append(B.T.dot(MUtPAxsW))
@@ -641,7 +649,8 @@ class ARAP:
 		for i in range(len(self.mesh.z)):
 			ans += self.constEsTerms[2][i].T.dot(c)*self.mesh.z[i]
 
-		return self.constEsTerms[0].T.dot(self.mesh.red_s) - ans
+		t1 =  self.constEsTerms[0].T.dot(self.mesh.red_s)
+		return t1- ans
 
 	def constTimeEgs(self):
 		c_vec = []
@@ -659,7 +668,7 @@ class ARAP:
 			Egs[i,:] = UtRtPAGi
 
 		cc = datetime.datetime.now()
-		print("TIME, ", (bb-aa).microseconds, (cc-bb).microseconds)
+		# print("TIME, ", (bb-aa).microseconds, (cc-bb).microseconds)
 		return Egs
 
 	def constTimeErs_mid(self):
@@ -729,17 +738,21 @@ class ARAP:
 		# 	dEdr[i] = DR[i].multiply(dEdR).sum()
 		dEdr = self.constTimeEr()
 
-		FPAx = self.mesh.GF.dot(self.PAx)#self.constTimeFPAx()
+		FPAx = self.constTimeFPAx() #self.mesh.GF.dot(self.PAx)
 		dEdg = self.PAG.T.dot(PAg - FPAx)
 
 		# UtPAx = self.mesh.GU.T.dot(self.PAx)
 		# RU = self.mesh.GR.dot(self.mesh.GU)
-		# dEdS =  self.sparseOuterProdDiags(self.mesh.GS.dot(UtPAx), UtPAx) -self.sparseOuterProdDiags(RU.T.dot(PAg), UtPAx)
+		# dEdS = self.sparseOuterProdDiags(self.mesh.GS.dot(UtPAx), UtPAx) -self.sparseOuterProdDiags(RU.T.dot(PAg), UtPAx)
 		# dEds = np.zeros(len(self.mesh.red_s))
 		# for i in range(len(dEds)):
 		# 	dEds[i] = DS[i].multiply(dEdS).sum()
-
+		
+		# print("deds")
+		# print(dEds)
 		dEds =self.constTimeEs()
+		# print(dEds)
+		# exit()
 
 		return dEdg, dEdr, dEds
 
@@ -830,7 +843,7 @@ class ARAP:
 
 	def dEdg(self):
 		PAg = self.PA.dot(self.mesh.getg())
-		FPAx = self.mesh.GF.dot(self.PAx)#self.constTimeFPAx()
+		FPAx = self.constTimeFPAx() #self.mesh.GF.dot(self.PAx)
 		res =  self.PAG.T.dot(PAg - FPAx)
 		return res
 
@@ -862,7 +875,6 @@ class ARAP:
 			m_PAg = np.reshape(one, (len(one)/2,2))
 			m_USUPAx = np.reshape(two, (len(two)/2,2))
 			F = np.matmul(m_PAg.T,m_USUPAx)
-
 			u, s, vt = np.linalg.svd(F, full_matrices=True)
 			R = np.matmul(vt.T, u.T)
 
@@ -881,7 +893,7 @@ class ARAP:
 		return 1
 
 	def itT(self):
-		FPAx = self.mesh.GF.dot(self.PAx)# self.constTimeFPAx()
+		FPAx = self.constTimeFPAx() #self.mesh.GF.dot(self.PAx)
 		
 		deltaAbtg = self.ANTI_BLOCK.T.dot(self.mesh.g)
 		GtAtPtFPAx = self.PAG.T.dot(FPAx)

Scripts/Mesh.py
@@ -303,8 +303,10 @@ class Mesh:
 		t_set = Set([i for i in range(len(self.T))])
 		if rclusters is False:
 			nrc =  len(self.T)
-			self.r_element_cluster_map = self.kmeans_rotationclustering(clusters=nrc)
-
+			if nrc == len(self.T):
+				self.r_element_cluster_map = np.arange(nrc)
+			else:
+				self.r_element_cluster_map = self.kmeans_rotationclustering(clusters=nrc)
 		for i in range(len(self.T)):			
 			self.r_cluster_element_map[self.r_element_cluster_map[i]].append(i)
 

Scripts/finiteDiff.py
@@ -4,10 +4,10 @@ import Arap
 import Neo
 import Display
 import Solvers
-np.set_printoptions(threshold="nan", linewidth=190, precision=8, formatter={'all': lambda x:'{:2.8f}'.format(x)})
+np.set_printoptions(threshold="nan", linewidth=190, precision=8, formatter={'all': lambda x:'{:2.5f}'.format(x)})
 
 def FiniteDifferencesARAP():
-	eps = 1e-6
+	eps = 1e-1
 	its = 100
 	# VTU,tofix = Meshwork.feather_muscle2_test_setup()
 	VTU = Meshwork.rectangle_mesh(x=2, y=2, step=0.1)
@@ -16,11 +16,9 @@ def FiniteDifferencesARAP():
 	mw.Mov = get_min(mw.V, a=1, eps=1e-2)
 	mesh = mw.getMesh()
 	arap = Arap.ARAP(imesh = mesh, filen="crap/")
-
 	E0 = arap.energy(_z=mesh.z, _R =mesh.GR, _S=mesh.GS, _U=mesh.GU)
 	print("Default Energy ", E0)
-	# print(mesh.red_s)
-	# exit()
+
 	def check_dEdg():
 		real = arap.dEdg()
 		dEdz = []
@@ -98,7 +96,7 @@ def FiniteDifferencesARAP():
 				Egg[i].append((Eij - Ei - Ej + E0)/(eps*eps))
 		
 		# print("Egg")
-		# print(Egg)
+		print(real)
 		print("Egg ", np.sum(np.array(Egg) - real))
 
 	def check_Hessian_dEdrdg():
@@ -129,7 +127,7 @@ def FiniteDifferencesARAP():
 
 				Erg[i].append((Eij - Ei - Ej + E0)/(eps*eps))
 
-		# print(real)
+		print(real)
 		print("Erg ",np.sum(np.array(Erg) - real))
 
 	def check_Hessian_dEdrdr():
@@ -163,7 +161,6 @@ def FiniteDifferencesARAP():
 	
 	def check_Hessian_dEdrds():
 		real = arap.Hess_Ers()
-
 		Ers = []
 		for i in range(len(mesh.red_r)):
 			Ers.append([])
@@ -258,28 +255,28 @@ def FiniteDifferencesARAP():
 		print("Egs ", np.linalg.norm(np.array(Egs) - real))
 
 	def check_dgds_drds():
-		Jac, real1, real2 = arap.Jacobian()
 
+
+		Jac, real1, real2 = arap.Jacobian()
 		dgds = []
 		drds = []
 
 		z0 = np.zeros(len(mesh.z)) + mesh.z
 		g0 = np.zeros(len(mesh.g)) + mesh.g
 		r0 = np.array(mesh.red_r) 
-		for i in range(len(mesh.red_s)):
+		for i in range(0,len(mesh.red_s)):
 			mesh.z = np.zeros(len(mesh.z)) + z0
 
 			mesh.red_s[i] += 0.5*eps
 			mesh.getGlobalF()
 			arap.updateConstUSUtPAx()
-
 			arap.iterate()
+			# print(mesh.red_r)
+			# print(arap.Energy())
+			# exit()
 			drds_left = np.array(mesh.red_r)
-			# if(mesh.reduced_g):
 			dgds_left =mesh.z + np.zeros(len(mesh.z))
-			# else:
-			# 	dgds_left =mesh.g + np.zeros(len(mesh.g))
-
+	
 			mesh.red_s[i] -= 0.5*eps
 			mesh.getGlobalF()
 			arap.updateConstUSUtPAx()
@@ -290,11 +287,8 @@ def FiniteDifferencesARAP():
 			arap.updateConstUSUtPAx()
 			arap.iterate()
 			drds_right = np.array(mesh.red_r)
-			# if(mesh.reduced_g):
 			dgds_right =mesh.z + np.zeros(len(mesh.z))
-			# else:
-			# 	dgds_right =mesh.g + np.zeros(len(mesh.g))
-
+	
 			mesh.red_s[i] += 0.5*eps
 			mesh.getGlobalF()
 			arap.updateConstUSUtPAx()
@@ -330,7 +324,7 @@ def FiniteDifferencesARAP():
 	# check_Hessian_dEdrdr()
 	# check_Hessian_dEdgds()
 	# check_Hessian_dEdrds()
-	# check_dgds_drds()
+	check_dgds_drds()
 
 FiniteDifferencesARAP()
 

Scripts/version3.py
@@ -16,12 +16,12 @@ from iglhelpers import *
 # F = igl.eigen.MatrixXi()
 # igl.readOFF("./MeshSetups/woman/mid_woman.off", V, F)
 # VTU = [np.array(e2p(V)[:,:2]/100.0), np.array(e2p(F))]
-VTU = Meshwork.rectangle_mesh(x=2, y=2, step=0.1)
-mw = Meshwork.Preprocessing(_VT = VTU)
-mw.Fix = Meshwork.get_max(mw.V, a=1, eps=1e-2)		
-mw.Mov = Meshwork.get_min(mw.V, a=1, eps=1e-2)
-mw.display()
-exit()
+# VTU = Meshwork.rectangle_mesh(x=2, y=2, step=0.1)
+# mw = Meshwork.Preprocessing(_VT = VTU)
+# mw.Fix = Meshwork.get_max(mw.V, a=1, eps=1e-2)		
+# mw.Mov = Meshwork.get_min(mw.V, a=1, eps=1e-2)
+# mw.display()X
+# exit()
 
 
 #Read-in mesh: read V, T, U, rot-clusters, skinning handles, Modes (if reduced)
@@ -40,5 +40,5 @@ ti = Solvers.TimeIntegrator(imesh = mesh, iarap = arap, ielastic = neo)
 #Running
 disp = Display.Display(isolve = ti)
 disp.headless()
-disp.display()
+# disp.display()
 